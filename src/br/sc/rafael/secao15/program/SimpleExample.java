9098ea962a08ea280ce63b6c41e33320dc389d80
Rafael Teixeira <rafaeltorress@gmail.com>
2021-07-06 22:27:48 -0300

    very bad solution

---
+++ src/br/sc/rafael/secao15/program/SimpleExample.java
@@ -0,0 +1,28 @@
+
+package br.sc.rafael.secao15.program;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SimpleExample {
+	public static void main(String[] args) {
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} 
+		catch (IOException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} 
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
+}
