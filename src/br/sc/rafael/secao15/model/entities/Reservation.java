a29e7f523a3191630fc7fb09df56ef0bbed684ff
Rafael Teixeira Silveira <rateixeira@uolinc.com>
2021-07-09 18:10:57 -0300

    good solution for exception

--- src/br/sc/rafael/secao15/model/entities/Reservation.java
+++ src/br/sc/rafael/secao15/model/entities/Reservation.java
@@ -1,22 +1,24 @@
 package br.sc.rafael.secao15.model.entities;
 
+import br.sc.rafael.secao15.model.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 
+
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
 
-	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/aaaa");
-
-	public Reservation() {
-
-	}
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -44,21 +46,27 @@ public class Reservation {
 	}
 
 	public void updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Reservation dates for update must be future dates");
+		}
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
 
+	@Override
 	public String toString() {
-		return "Room" 
-			+ roomNumber 
-			+ ", checkin: " 
-			+ sdf.format(checkIn) 
-			+ ", checkout: " 
-			+ sdf.format(checkOut) 
-			+ ", "
-			+ duration() 
-			+ " nights";
-
+		return "Room "
+				+ roomNumber
+				+ ", check-in: "
+				+ sdf.format(checkIn)
+				+ ", check-out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nights";
 	}
-
 }
